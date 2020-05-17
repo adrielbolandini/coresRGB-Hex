9944a60f44e3bfb761ed7d29ebb858b58e173026
testtzlaffe <50345995+testtzlaffe@users.noreply.github.com>
2020-05-16 23:44:09 -0300

    Ajustes no main.js

--- main.js
+++ main.js
@@ -7,56 +7,69 @@ const Bnumber = document.querySelector('#Bnumber');
 const quadrado = document.querySelector('#cor');
 const hexa = document.querySelector('#hex');
 
-function passavalor(){
-    Rnumber.value=Rrange.value;
-    Gnumber.value=Grange.value;
-    Bnumber.value=Brange.value;
-    mudarcor();
+// iniciando input text com valor do range
+Rnumber.value = Rrange.value;
+Gnumber.value = Grange.value;
+Bnumber.value = Brange.value;
+
+function passavalor() {
+  Rnumber.value = Rrange.value;
+  Gnumber.value = Grange.value;
+  Bnumber.value = Brange.value;
+  mudarcor();
 }
 
-function passarange(){
-    Rrange.value=Rnumber.value;
-    Grange.value=Gnumber.value;
-    Brange.value=Bnumber.value;
-    mudarcor();
+function passarange() {
+  Rrange.value = Rnumber.value;
+  Grange.value = Gnumber.value;
+  Brange.value = Bnumber.value;
+  mudarcor();
 }
 
-function mudarcor(){
+function mudarcor() {
+  //tratando os valores negativos, além dos maiores de 255
+  function normalize() {
+    Rnumber.value =
+      Rnumber.value > 255 ? 255 : Rnumber.value < 0 ? 0 : Rnumber.value;
+    Gnumber.value =
+      Gnumber.value > 255 ? 255 : Gnumber.value < 0 ? 0 : Gnumber.value;
+    Bnumber.value =
+      Bnumber.value > 255 ? 255 : Bnumber.value < 0 ? 0 : Bnumber.value;
+  }
 
-    function normalize(){
-        if (Rnumber.value>255){
-            Rnumber.value=255;
-        }
-        if(Gnumber.value>255){
-            Gnumber.value=255;
-        }
-        if(Bnumber.value>255){
-            Bnumber.value=255;
-        }
-    }
+  let r = Rnumber.value || 0;
+  let g = Gnumber.value || 0;
+  let b = Bnumber.value || 0;
 
-    let r = Rnumber.value||0;
-    let g = Gnumber.value||0;
-    let b = Bnumber.value||0;
+  function colorir() {
+    // template literals (funcionou no meu chrome)
+    var cor = `rgb(${r},${g},${b})`;
+    quadrado.style.backgroundColor = cor;
+  }
+  function hex() {
+    hexa.value =
+      '#' + ((1 << 24) + (+r << 16) + (+g << 8) + +b).toString(16).slice(1);
+  }
 
-    function colorir(){
-        var cor =  'rgb('+r+',' +g+',' +b+')';
-        quadrado.style.backgroundColor = cor;
-    }
-    function hex(){
-        hexa.value= "#"+((1<<24)+((+r)<<16)+((+g)<<8)+(+b)).toString(16).slice(1);
-    }
-
-    normalize(),colorir(),hex();
+  normalize(), colorir(), hex();
 }
 
-
-
-
-Rrange.addEventListener('input', function(){passavalor()});
-Grange.addEventListener('input', function(){passavalor()});
-Brange.addEventListener('input', function(){passavalor()});
-Rnumber.addEventListener('input', function(){passarange()});
-Gnumber.addEventListener('input', function(){passarange()});
-Bnumber.addEventListener('input', function(){passarange()});
+Rrange.addEventListener('input', function () {
+  passavalor();
+});
+Grange.addEventListener('input', function () {
+  passavalor();
+});
+Brange.addEventListener('input', function () {
+  passavalor();
+});
+Rnumber.addEventListener('input', function () {
+  passarange();
+});
+Gnumber.addEventListener('input', function () {
+  passarange();
+});
+Bnumber.addEventListener('input', function () {
+  passarange();
+});
 mudarcor();
